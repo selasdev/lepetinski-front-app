68015d90626604bccb49d0ea5d2f94b895e02f11
Carlos Rosales <carlosros033@gmail.com>
2022-08-02 20:43:57 -0400

    feat: differentiating home components

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { BackgroundBanner } from '../../components/organisms/BackgroundBanner'
-import { Categories } from '../../components/organisms/Categories'
-import { Hero } from '../../components/organisms/Hero'
-import Navbar from '../../components/organisms/Navbar'
-import { useAuthenticationProvider } from '../../contexts/Auth/context'
+import React from "react";
+import { BackgroundBanner } from "../../components/organisms/BackgroundBanner";
+import { Categories } from "../../components/organisms/Categories";
+import { Hero } from "../../components/organisms/Hero";
+import Navbar from "../../components/organisms/Navbar";
+import { useAuthenticationProvider } from "../../contexts/Auth/context";
 
 const HomeView = () => {
-  const { authenticated } = useAuthenticationProvider()
+  const { authenticated } = useAuthenticationProvider();
 
+  return authenticated ? <LoggedInHome /> : <LoggedOutHome />;
+};
+
+export const LoggedOutHome = () => {
   return (
     <React.Fragment>
       <Navbar />
       <BackgroundBanner />
-      {!authenticated ? <Hero /> : null}
+      <Hero />
       <Categories />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default HomeView
+export const LoggedInHome = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <BackgroundBanner />
+      <Categories />
+    </React.Fragment>
+  );
+};
+
+export default HomeView;
