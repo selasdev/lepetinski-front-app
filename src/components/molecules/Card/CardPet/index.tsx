f4e31c94cb73c805e44a6ee1f5f8ec3b08afa9b9
Carlos Rosales <carlosros033@gmail.com>
2022-08-07 18:25:44 -0400

    feat: saved animals section

--- src/components/molecules/Card/CardPet/index.tsx
+++ src/components/molecules/Card/CardPet/index.tsx
@@ -1,39 +1,83 @@
-import { Card, CardMedia, CardContent, Typography, Stack, Box } from '@mui/material'
-import { ICardPet } from './types'
-import { useNavigate } from 'react-router'
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Stack,
+  Box,
+} from "@mui/material";
+import { ICardPet } from "./types";
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import styled from "styled-components";
+import { SavedPetDialog } from "./dialog";
 
-export const CardPet = ({ img, name, features, address }: ICardPet) => {
-  const navigate = useNavigate()
+const StyledCard = styled(Card)`
+  box-shadow: 0px 3px 8px -1px rgba(50, 50, 71, 0.25) !important;
+  filter: drop-shadow(0px 0px 1px rgba(12, 26, 75, 0.24)) !important;
+  border-radius: 16px !important;
+`;
+
+export const CardPet = ({
+  img,
+  name,
+  features,
+  address,
+  saved = false,
+}: ICardPet): JSX.Element => {
+  const navigate = useNavigate();
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+
+  const cardClickCallback = () => {
+    if (!saved) {
+      navigate("/pet-adopt");
+    } else {
+      setDialogOpen(true);
+    }
+  };
 
   return (
-    <Card onClick={() => navigate('/pet-adopt')}>
-      <CardContent>
-        <CardMedia
-          component='img'
-          height='140'
-          image={img}
-          alt='pet'
-          sx={() => ({
-            width: '100%',
-            borderRadius: '15px'
-          })}
-        />
+    <>
+      <StyledCard onClick={cardClickCallback}>
         <CardContent>
-          <Typography gutterBottom variant='h5' component='div'>
-            {name}
-          </Typography>
-          <Stack direction='row' divider={<Box margin='0 5px'> - </Box>}>
-            {features?.map((feature, idx) => (
-              <Typography variant='body2' color='text.secondary' key={idx + feature}>
-                {feature}
-              </Typography>
-            ))}
-          </Stack>
-          <Typography variant='body2' color='text.secondary'>
-            {address}
-          </Typography>
+          <CardMedia
+            component="img"
+            height="140"
+            image={img}
+            alt="pet"
+            sx={() => ({
+              width: "100%",
+              borderRadius: "15px",
+            })}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {name}
+            </Typography>
+            <Stack direction="row" divider={<Box margin="0 5px"> - </Box>}>
+              {features?.map((feature, idx) => (
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  key={idx + feature}
+                >
+                  {feature}
+                </Typography>
+              ))}
+            </Stack>
+            <Typography variant="body2" color="text.secondary">
+              {address}
+            </Typography>
+          </CardContent>
         </CardContent>
-      </CardContent>
-    </Card>
-  )
-}
+      </StyledCard>
+      <SavedPetDialog
+        open={dialogOpen}
+        name={name}
+        onClose={() => {
+          setDialogOpen(false);
+        }}
+      />
+    </>
+  );
+};
